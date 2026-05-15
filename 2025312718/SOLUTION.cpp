de41acd59089f4072b5eacd86c00fd64f890a4c1
sunggyulee951 <63052246+sunggyulee951@users.noreply.github.com>
2026-05-15 20:39:56 +0900

    Add files via upload

---
+++ 2025312718/SOLUTION.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+struct Card {
+    char fruit;
+    int cnt;
+};
+
+const int MAX = 250000;
+
+Card deckA[MAX], deckB[MAX];
+Card center[MAX];
+
+int frontA = 0, backA = 0;
+int frontB = 0, backB = 0;
+int centerSize = 0;
+
+bool canRing(Card topA, Card topB) {
+    int b = 0, c = 0, g = 0, s = 0;
+
+    if (topA.fruit == 'b') b += topA.cnt;
+    else if (topA.fruit == 'c') c += topA.cnt;
+    else if (topA.fruit == 'g') g += topA.cnt;
+    else if (topA.fruit == 's') s += topA.cnt;
+
+    if (topB.fruit == 'b') b += topB.cnt;
+    else if (topB.fruit == 'c') c += topB.cnt;
+    else if (topB.fruit == 'g') g += topB.cnt;
+    else if (topB.fruit == 's') s += topB.cnt;
+
+    return b == 5 || c == 5 || g == 5 || s == 5;
+}
+
+void skipLineEnd() {
+    while (cin.peek() == ' ') cin.get();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    char fruit;
+    int cnt;
+
+    // A 덱 입력
+    while (cin >> fruit >> cnt) {
+        deckA[backA++] = {fruit, cnt};
+
+        skipLineEnd();
+        if (cin.peek() == '\n') {
+            cin.get();
+            break;
+        }
+    }
+
+    // B 덱 입력
+    while (cin >> fruit >> cnt) {
+        deckB[backB++] = {fruit, cnt};
+
+        skipLineEnd();
+        if (cin.peek() == '\n' || cin.peek() == EOF) {
+            break;
+        }
+    }
+
+    Card topA = {0, 0};
+    Card topB = {0, 0};
+
+    int turn = 0; // 0: A, 1: B
+
+    for (int move = 0; move < N; move++) {
+        if (turn == 0) {
+            if (frontA >= backA) break;
+
+            Card cur = deckA[frontA++];
+            topA = cur;
+            center[centerSize++] = cur;
+
+            if (frontA >= backA) break;
+
+            if (canRing(topA, topB)) {
+                for (int i = 0; i < centerSize; i++) {
+                    deckA[backA++] = center[i];
+                }
+
+                centerSize = 0;
+                topA = {0, 0};
+                topB = {0, 0};
+            }
+
+            turn = 1;
+        } else {
+            if (frontB >= backB) break;
+
+            Card cur = deckB[frontB++];
+            topB = cur;
+            center[centerSize++] = cur;
+
+            if (frontB >= backB) break;
+
+            if (canRing(topA, topB)) {
+                for (int i = 0; i < centerSize; i++) {
+                    deckB[backB++] = center[i];
+                }
+
+                centerSize = 0;
+                topA = {0, 0};
+                topB = {0, 0};
+            }
+
+            turn = 0;
+        }
+    }
+
+    cout << backA - frontA << ' ' << backB - frontB << '\n';
+
+    return 0;
+}
